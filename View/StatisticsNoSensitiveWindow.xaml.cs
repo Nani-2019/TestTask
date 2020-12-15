47273ad6ac8e7cbb12edbc2e0668204c1f389666
Nikita <nikitakozub23112017@gmail.com>
2020-12-15 07:55:23 +0700

    end

--- View/StatisticsNoSensitiveWindow.xaml.cs
+++ View/StatisticsNoSensitiveWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ViewModel.Statistics;
 
 namespace View
 {
@@ -23,6 +24,7 @@ namespace View
         public StatisticsNoSensitiveWindow()
         {
             InitializeComponent();
+            DataContext = new StatisticsNoSensitiveViewModel();
         }
     }
 }
